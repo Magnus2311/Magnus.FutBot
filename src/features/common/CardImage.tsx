f2137a570bc1364209aa25723a29d0e4b5451e09
yavor.orlyov <yavor.orlyov@radostinbogdanovsmartit.onmicrosoft.com>
2022-09-12 18:25:46 +0300

    Adjusted image

--- src/features/common/CardImage.tsx
+++ src/features/common/CardImage.tsx
@@ -10,22 +10,36 @@ export const CardImage = ({ card }: Props) => {
     getCardImages(card);
 
   return (
-    <div style={{ position: "relative" }}>
+    <div style={{ position: "relative", scale: "0.5" }}>
       <img
         src={revisionImg}
         style={{ position: "absolute", top: 0, left: 0 }}
       />
       <img
         src={clubImg}
-        style={{ position: "absolute", top: 405, left: 105 }}
+        style={{ position: "absolute", top: 250, left: 100, scale: "0.8" }}
       />
       <img
         src={flagImg}
-        style={{ position: "absolute", top: 560, left: 123 }}
+        style={{ position: "absolute", top: 400, left: 115 }}
       />
       {/* <img src={leagueImg} style={{ position: "absolute", top: 0, left: 0 }} /> */}
-      <img src={playerImg} style={{ position: "absolute", top: 0, left: 0 }} />
-      <p>{card.name}</p>
+      <img
+        src={playerImg}
+        style={{ scale: "1.6", position: "absolute", top: 200, left: 320 }}
+      />
+      <p
+        style={{
+          position: "absolute",
+          top: "500px",
+          left: "104px",
+          fontSize: "80px",
+          fontWeight: 400,
+          color: "gold",
+        }}
+      >
+        {card.name}
+      </p>
       <p
         style={{
           position: "absolute",
