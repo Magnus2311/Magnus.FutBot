ffb4e9ad218f1c63fb0e7d5473a1011b53314b8a
yavor.orlyov <yavor.orlyov@radostinbogdanovsmartit.onmicrosoft.com>
2022-09-14 13:01:10 +0300

    Removed reactstrap dependencies

--- src/features/authentication/NotLoggedNavMenu.tsx
+++ src/features/authentication/NotLoggedNavMenu.tsx
@@ -1,16 +1,22 @@
 ﻿import React from "react";
 import { Link } from "react-router-dom";
-import { NavItem, NavLink } from "reactstrap";
+import { NavItem, NavLink } from "react-bootstrap";
 
 const NotLoggedNavMenu = () => {
-    return <>
-        <NavItem>
-            <NavLink tag={Link} className="text-dark" to="/auth/registration">Register</NavLink>
-        </NavItem>
-        <NavItem>
-            <NavLink tag={Link} className="text-dark" to="/auth/login">Login</NavLink>
-        </NavItem>
+  return (
+    <>
+      <NavItem>
+        <NavLink as={Link} className="text-dark" to="/auth/registration">
+          Register
+        </NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink as={Link} className="text-dark" to="/auth/login">
+          Login
+        </NavLink>
+      </NavItem>
     </>
-}
+  );
+};
 
 export default NotLoggedNavMenu;
