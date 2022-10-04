91aa9770e51d9b9f5e84ff346e3da035cf03fdde
Iavor Orlyov <iavor_orlyov@yahoo.com>
2022-10-04 22:47:50 +0300

    trying to get images from azure

--- src/helpers/images.ts
+++ src/helpers/images.ts
@@ -1,6 +1,34 @@
 import { Card } from "../models/models";
+import { BlobServiceClient, ContainerClient } from "@azure/storage-blob";
 
 export const getCardImages = (card: Card) => {
+  const containerName = `newcontainer`;
+  const sasToken = process.env.REACT_APP_STORAGESASTOKEN;
+  const storageAccountName = process.env.REACT_APP_STORAGERESOURCENAME;
+
+  const blobService = new BlobServiceClient(sasToken!);
+  const containerClient: ContainerClient =
+    blobService.getContainerClient(containerName);
+
+  const getBlobsInContainer = async (containerClient: ContainerClient) => {
+    const returnedBlobUrls: string[] = [];
+
+    // get list of blobs in container
+    // eslint-disable-next-line
+    for await (const blob of containerClient.listBlobsFlat()) {
+      // if image is public, just construct URL
+      returnedBlobUrls.push(
+        `https://${storageAccountName}.blob.core.windows.net/${containerName}/${blob.name}`
+      );
+    }
+
+    return returnedBlobUrls;
+  };
+
+  getBlobsInContainer(containerClient).then((blobs) => {
+    debugger;
+  });
+
   try {
     return {
       revisionImg: require(`../../../assets/revision-images/${card.revision}.png`),
