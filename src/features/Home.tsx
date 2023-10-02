121d79199ac77b518ff868f8d2806d4cdcc1cdcb
Yavor Orlyov <microinvest@hotmail.com>
2023-10-02 13:41:17 +0300

    Описание на потенциални trades

--- src/features/Home.tsx
+++ src/features/Home.tsx
@@ -1,5 +1,15 @@
 const Home = () => {
-  return <div>HOME SCREEN</div>;
+  return <div>
+    <div>
+      Понеделник - Fodders към 18:00 пускат Upgrade
+    </div>
+    <div>
+      При пускане на SBC, ако е с добър vote във Futbin, отваря се страницата за създаване на отбор и се търсят добри линкове за него
+    </div>
+    <div>
+      Следят се играчи от популярни SBC-та, които през време се качват (Gold Non-Rare)
+    </div>
+    </div>;
 };
 
 export default Home;
